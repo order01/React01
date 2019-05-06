3481fd9b105987253c91c50fb6459672ec67625b
order01 <1782499485@qq.com>
2019-05-06 16:40:58 +0800

    脚手架

---
+++ react-demo/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {App,Test1} from './App';
+import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router } from "react-router-dom";
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Test1/>, document.getElementById('root1'))
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
